0a3fd672503c0b044be4f0d72ed0479d7a6e881c
Vincent Davis <vincent@heteroskedastic.net>
2023-01-12 14:54:19 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# pyfitness_streamlit
+# Welcome to Vincent's experiments with cycling data files
+
+This "dashboard" will look at the details of the FIT file 
+and the distribution on the data. You are able to compare two file side by side.
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import plotly.express as px
 """
 # Welcome to Vincent's experiments with cycling data files
 
-This "dashboard will look at the details of the FIT file 
+This "dashboard" will look at the details of the FIT file 
 and the distribution on the data. You are able to compare two file side by side.
 """
 
