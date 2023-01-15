906305ad547fc6d82a885719ab2fb0b8666bae21
Vincent Davis <vincent@heteroskedastic.net>
2023-01-15 12:37:18 -0700

    add drivetrain loss

--- pages/2_WAM Estimated VS Measures.py
+++ pages/2_WAM Estimated VS Measures.py
@@ -1,10 +1,11 @@
 import pandas as pd
-import streamlit as st
-from pyfitness.load_data import fit2df
-# from vam import FitVam
-from vam2 import estimated_power
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit as st
+from pyfitness.load_data import fit2df
+
+# from vam import FitVam
+from vam2 import estimated_power
 
 """# Currently in development
 Feel free to give it a try, but it is not ready for prime time yet.
@@ -15,14 +16,14 @@ if fit_file is not None:
     df['seconds'] = pd.to_datetime(df.index).astype(int) / 10 ** 9
     df['seconds'] = (df['seconds'] - df['seconds'].min()).astype(int)
 
-
     st.write("#### Enter start and end times")
     st.write("The time values are used to select the segment of the ride to analyze")
 
     c1, c2, c3, c4, c5 = st.columns(5)
     with c1:
         start_time = st.number_input('Start time sec:', min_value=0, max_value=df['seconds'].max(), value=0, step=1)
-        end_time = st.number_input('End time sec:', min_value=0, max_value=df['seconds'].max(), value=df['seconds'].max(),
+        end_time = st.number_input('End time sec:', min_value=0, max_value=df['seconds'].max(),
+                                   value=df['seconds'].max(),
                                    step=1)
     with c2:
         rider_weight = st.number_input('Rider weight kg:', min_value=0, max_value=200, value=70, step=1)
@@ -33,14 +34,18 @@ if fit_file is not None:
     with c4:
         temperature = st.number_input('Temperature C:', min_value=-20, max_value=50, value=20, step=1)
         drag_coefficient = st.number_input('Drag coefficient:', min_value=0.0, max_value=1.0, value=0.8, step=0.05)
+        drive_train = st.number_input('Drive train loss:', min_value=0.0, max_value=1.0, value=0.05, step=0.01)
     with c5:
         frontal_area = st.number_input('Frontal area m^2:', min_value=0.0, max_value=2.0, value=0.565, step=0.1)
-        rolling_resistance = st.number_input('Rolling resistance:', min_value=0.0, max_value=0.1, value=0.0005, step=0.0001)
+        rolling_resistance = st.number_input('Rolling resistance:', min_value=0.0000, max_value=0.1, value=0.0005,
+                                             step=0.0001, format="%.4f")
         roll = st.number_input('VAM smooting:', min_value=0, max_value=30, value=5, step=1)
 
-    fitted = estimated_power(df=df, start_time=start_time, end_time=end_time, rider_weight=rider_weight, bike_weight=bike_weight,
-                wind_speed=wind_speed, wind_direction=wind_direction, temperature=temperature,
-                drag_coefficient=drag_coefficient, frontal_area=frontal_area, rolling_resistance=rolling_resistance, roll=roll)
+    fitted = estimated_power(df=df, start_time=start_time, end_time=end_time, rider_weight=rider_weight,
+                             bike_weight=bike_weight,
+                             wind_speed=wind_speed, wind_direction=wind_direction, temperature=temperature,
+                             drag_coefficient=drag_coefficient, frontal_area=frontal_area,
+                             rolling_resistance=rolling_resistance, roll=roll)
 
     filtered_df = fitted[(fitted['seconds'] >= start_time) & (fitted['seconds'] <= end_time)]
     filtered_df['roll_vam'] = filtered_df['vam'].rolling(roll).mean()
@@ -52,8 +57,9 @@ if fit_file is not None:
     pvam_fig = go.Figure()
     pvam_fig.update_layout(
         title='Measured vs Estimated VAM Power')
-    pvam_fig.add_trace(go.Scatter(name="Est VAM Power", x = filtered_df['seconds'], y = filtered_df['vam_power'].rolling(roll).mean()))
-    pvam_fig.add_trace(go.Scatter(name="Power", x = filtered_df['seconds'], y = filtered_df['power'].rolling(roll).mean()))
+    pvam_fig.add_trace(go.Scatter(name="Est VAM Power", x=filtered_df['seconds'],
+                                  y=filtered_df['vam_power'].rolling(roll).mean() * (1 - drive_train)))
+    pvam_fig.add_trace(go.Scatter(name="Power", x=filtered_df['seconds'], y=filtered_df['power'].rolling(roll).mean()))
     # pvam_fig = px.line(x = filtered_df['seconds'], y = filtered_df['vam_power'].rolling(roll).mean(), title='Est Power', label='Est Power')
     # pvam_fig.add_scatter(x=filtered_df['seconds'], y=filtered_df['power'].rolling(roll).mean(), mode='lines', name='Actual Power')
 
@@ -65,15 +71,12 @@ if fit_file is not None:
     comp_fig = go.Figure()
     comp_fig.add_trace(
         go.Scatter(name="Air", x=filtered_df['seconds'], y=filtered_df['air_drag'].rolling(roll).mean()))
-    comp_fig.add_trace(go.Scatter(name="Climbing", x=filtered_df['seconds'], y=filtered_df['climbing_force'].rolling(roll).mean()))
-    comp_fig.add_trace(go.Scatter(name="Rolling", x=filtered_df['seconds'], y=filtered_df['rolling_force'].rolling(roll).mean()))
+    comp_fig.add_trace(
+        go.Scatter(name="Climbing", x=filtered_df['seconds'], y=filtered_df['climbing_force'].rolling(roll).mean()))
+    comp_fig.add_trace(
+        go.Scatter(name="Rolling", x=filtered_df['seconds'], y=filtered_df['rolling_force'].rolling(roll).mean()))
+    comp_fig.add_trace(
+        go.Scatter(name="Acceleration", x=filtered_df['seconds'], y=filtered_df['speed_change'].rolling(roll).mean()))
     pvam_fig.update_layout(
         title='Components or Estimated VAM Power')
     st.plotly_chart(comp_fig, theme="streamlit", use_container_width=True)
-
-
-
-
-
-
-
--- vam2.py
+++ vam2.py
@@ -1,14 +1,12 @@
-from math import exp, cos, sin, radians, atan
+from math import exp, radians
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 def estimated_power(df: pd.DataFrame, start_time: int, end_time: int, rider_weight: float, bike_weight: float,
                     wind_speed: float, wind_direction: int, temperature: float, drag_coefficient: float,
                     frontal_area: float, rolling_resistance: float, roll: int) -> pd.DataFrame:
-
-    CdA = drag_coefficient * frontal_area
-    altitude = (df.altitude.max() - df.altitude.min()) / 2
     if 'enhanced_altitude' in df.columns:
         df['slope'] = df.enhanced_altitude.diff() / df.distance.diff()
     else:
@@ -19,18 +17,23 @@ def estimated_power(df: pd.DataFrame, start_time: int, end_time: int, rider_weig
         pass
     else:
         df['speed'] = df.distance.diff() / df.time.diff()
-    if 'seconds' not in df.columns :
+    if 'seconds' not in df.columns:
         df['seconds'] = pd.to_datetime(df.index, unit='s', origin='unix').astype(int) // 10 ** 9
         df['seconds'] = df['seconds'] - df.seconds.min()
+
     df['vam'] = (df.altitude.diff() / df.seconds.diff()) * 3600
     df['roll_vam'] = df['vam'].rolling(roll).mean()
-    df['climb_force'] = bike_weight + rider_weight * 9.0867 * np.sin(np.arctan(df.slope))
+
+    CdA = drag_coefficient * frontal_area
+    altitude = (df.altitude.max() - df.altitude.min()) / 2
     df['air_density'] = ((101325 / (287.05 * 273.15)) * (273.15 / (temperature + 273.15)) *
-                                  exp((-101325 / (287.05 * 273.15)) * 9.8067 * (altitude / 1013.25)))
+                         exp((-101325 / (287.05 * 273.15)) * 9.8067 * (altitude / 1013.25)))
     df['effective_wind_speed'] = np.cos(radians(wind_direction)) * wind_speed
     df['air_drag'] = (0.5 * CdA * df.air_density * (df.speed / (3.6 + df.effective_wind_speed)) ** 2) * df.speed
     df['climbing_force'] = (bike_weight + rider_weight * 9.0867 * np.sin(np.arctan(df.slope))) * df.speed
-    df['rolling_force'] = (np.cos(np.arctan(df.slope)) * 9.8067 * (bike_weight + rider_weight) * rolling_resistance) * df.speed
-    df['vam_power'] = (df[['air_drag', 'climbing_force', 'rolling_force']].sum(axis='columns'))
+    df['rolling_force'] = (np.cos(np.arctan(df.slope)) * 9.8067 * (
+                bike_weight + rider_weight) * rolling_resistance) * df.speed
+    df['speed_change'] = (bike_weight + rider_weight) * (df.speed.diff() / df.seconds.diff()) * df.speed
+    df['vam_power'] = (df[['air_drag', 'climbing_force', 'rolling_force', 'speed_change']].sum(axis='columns'))
     return df
 
