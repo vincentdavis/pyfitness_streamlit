b2152ab8f9a0800e2203a5171d54dbaeb0411be6
Vincent Davis <vincent@heteroskedastic.net>
2023-01-19 14:17:07 -0700

    fix link

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 
 This is a work in progress. Please report any issues at [pyfitness_streamlit](https://github.com/vincentdavis/pyfitness_streamlit)
 
-You can also contact me on discord: [Vincent](discordapp.com/users/VincentDavis#3484
+You can also contact me on discord: [Vincent](discordapp.com/users/VincentDavis#3484)
 
 This "dashboard" will look at the details of the FIT file 
 and the distribution on the data. You are able to compare two file side by side.
