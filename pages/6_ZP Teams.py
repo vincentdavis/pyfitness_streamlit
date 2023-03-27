5d230212a3257a6d85cda31c45123b1957cb71df
Vincent Davis <vincent@heteroskedastic.net>
2023-03-27 09:03:18 -0600

    add team export

--- pages/6_ZP Teams.py
+++ pages/6_ZP Teams.py
@@ -1,9 +1,7 @@
-from time import sleep
 
-import pandas as pd
 import streamlit as st
 
-from zp_teams import team_list, team_overlap_by_rider
+from zp_teams import team_riders
 
 st.title("ZP Team List")
 """ # Teams and Riders
@@ -13,14 +11,35 @@ Please report any issues at [pyfitness_streamlit](https://github.com/vincentdavi
 
 You can also contact me on discord: [Vincent](discordapp.com/users/VincentDavis#3484)
 """
-teams = team_list()
-df = teams['teams_df']
-df.sort_values('riders', ascending=False, inplace=True)
-st.dataframe(teams['teams_df'])
-st.write(f"Total teams: {len(df)}")
+st.markdown("**Export team list**")
+st.write("Enter a full team url, such as https://zwiftpower.com/team.php?id=999999")
+team_url = st.text_input(label="Team URL", placeholder="Team URL")
+if 'https://zwiftpower.com/team.php?id=' in team_url:
+    id = team_url.split("=")[-1]
+    try:
+        assert id != '999999'
+        int(id) >= 1
+    except:
+        st.write("Please enter a valid team id")
+        raise
+    st.write("Getting ZwiftPower data, wait for it.... Might take 5-10 seconds")
+    result = team_riders(id)
+    st.download_button(label="Download csv file",
+                       data=result['team_riders_df'].to_csv(index=False).encode('utf-8'),
+                       file_name=f"Team_ID_{id}.csv",
+                       mime='text/csv')
 
-overlaps = team_overlap_by_rider(df, include=None, depth=20)
-dt = overlaps['team_overlap']
-st.write(f"Only inspecting the largest 5 teams at this time")
-st.write(f"Only listing riders on more then 1 team")
-st.dataframe(dt.loc[dt['team_count'] > 1])
+    st.dataframe(result['team_riders_df'])
+
+
+    # teams = team_list()
+    # df = teams['teams_df']
+    # df.sort_values('riders', ascending=False, inplace=True)
+    # st.dataframe(teams['teams_df'])
+    # st.write(f"Total teams: {len(df)}")
+    #
+    # overlaps = team_overlap_by_rider(df, include=None, depth=20)
+    # dt = overlaps['team_overlap']
+    # st.write(f"Only inspecting the largest 5 teams at this time")
+    # st.write(f"Only listing riders on more then 1 team")
+    # st.dataframe(dt.loc[dt['team_count'] > 1])
--- zp_teams.py
+++ zp_teams.py
@@ -1,9 +1,13 @@
 import json
 from time import sleep
-from zp import ZwiftLogin
-from spaces import get_space
-import streamlit as st
+
 import pandas as pd
+import streamlit as st
+
+from spaces import get_space
+from zp import ZwiftLogin
+
+
 def team_list():
     """Get the list of teams
     https://zwiftpower.com/api3.php?do=team_list
@@ -20,15 +24,31 @@ def team_list():
         print(f"failed to write json: {e}")
     return results
 
-def team_riders(team_id):
+
+def team_riders(team_id: int | str) -> dict[str, pd.DataFrame]:
     """Get the list of team members
     https://zwiftpower.com/api3.php?do=team_riders&id=2707
     """
     api_url = {'team_riders': f"https://zwiftpower.com/api3.php?do=team_riders&id={team_id}"}
     z = ZwiftLogin()
     results = z.get(api_url)
+    results['team_riders_df']['ftp'] = results['team_riders_df']['ftp'].apply(lambda x: x[0])
+    results['team_riders_df']['w'] = results['team_riders_df']['w'].apply(lambda x: x[0])
+    div_map = {40: 'A', 30: 'B', 20: 'C', 10: 'D', 0: '-', 5: 'A+'}
+    results['team_riders_df']['cat'] = results['team_riders_df']['div'].map(div_map)
+    results['team_riders_df']['catw'] = results['team_riders_df']['divw'].map(div_map)
 
+    def gender(x: pd.DataFrame) -> str:
+        if x['div'] >= 0 and x['divw'] == 0:
+            return 'M'
+        elif x['divw'] >= 0:
+            return 'F'
+        else:
+            return '-'
+
+    results['team_riders_df']['gender'] = results['team_riders_df'].apply(lambda x: gender(x), axis=1)
     return results
+
 
 def save_record(path, record):
     """
@@ -42,16 +62,19 @@ def save_record(path, record):
     except Exception as e:
         print(f"Failed to get team members: {e}")
 
+
 def team_refresh():
     """Refresh the team list and team riders"""
     teams = team_list()
     for t in teams['teams_json']['data']:
         team_riders(t['team_id'])
         sleep(.5)
+
+
 def team_overlap_by_rider(team_df: pd.DataFrame, include: int = None, depth: int = 20) -> dict:
     riders = {}
     riders_teams = {}
-    teams = team_df.iloc[0:depth].to_dict('records') # How many team to look at
+    teams = team_df.iloc[0:depth].to_dict('records')  # How many team to look at
     if include is not None:
         teams.update(team_df.iloc[team_df['team_id'] == include].to_dict('records'))
     for t in team_df.iloc[0:depth].to_dict('records'):
