78a8efd6d5ce2642aec32e24406f23a9c9c92861
Vincent Davis <forklift@vdavis.net>
2024-02-04 13:14:57 -0700

    start experiment work

---
+++ pages/7_Marginal Gain Experiments.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Fit File info",
+    page_icon="\u1F6B2",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+"""
+### Marginal Gain Experiments
+
+#### Multiple setting for comparing FIT files from experimental setups.
+
+
+Questions, comments, contact me on discord: [Vincent.Davis](discordapp.com/users/vincent.davis)
+"""
