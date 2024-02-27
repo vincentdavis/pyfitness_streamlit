327d3bd5928e7609c0c096fbd2e20ffa69f1877f
Vincent Davis <forklift@vdavis.net>
2024-02-26 21:28:16 -0700

    add worst ramp test

---
+++ pages/8_Worst Ramp Ever.py
@@ -0,0 +1,101 @@
+import streamlit as st
+
+from ramp_test import make_zwo_from_ramp, ramp_test_activity
+
+st.set_page_config(
+    page_title="Worst Ramp Ever",
+    page_icon="\u1F6B2",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+"""
+## The Worst Ramp Test Ever
+Questions, comments, contact me on discord: [Vincent.Davis](discordapp.com/users/vincent.davis)
+
+The goal of this project was to take a riders power curve, sometimes called critical power curve and build a workout 
+that will produce the that power curve. My initial question was, "Can I build a 20min workout that will produce my 20min 
+power profile?" The answer is yes.
+
+As you can image, a 20min workout that will producce your 20min profile, is never going to be easy. What this looks like 
+will surprise you.
+
+### How the problem is solved:
+1. Define a power profile, it must start with 1 second power and end with 20min and contain as many intermidiat points 
+are desired. Something like: `(sec, power) (1, 1000), (2, 900) (3, 875), (5, 800), (30, 500), (60, 450), (300, 400), (1200, 350)`
+2. Interperlate the power profile for each second. Keep in mind, (5, 800) means 800 watts(average) for 5 seconds.
+3. We are now ready to calculate the workout.
+
+### The workout:
+1. We start from the end of the workout and work toward the start.
+2. The end power, last second, of the workout will be the riders 1 sec critical power = 1000.
+3. The next (second to last) will need to statisfy (1000w + x)/2sec = 900w. x = 800w To make sense of this, if you did 
+1000w for 1 second than to have a 2 second average power of 900watts, you only need to do 800watts for the second, 
+second.
+4. The third is than, (1000w + 800w + x)/3sec = 875w. x = 825w
+5. Continue this process until the workout is complete.
+6. Now we have ramp power defined for each second of duration from 1-1200seconds
+7. The last 30seconds is a 1 second ramp.
+8. Average the power for the second to last 30seconds. as a segment. 
+9. The first 19min is converted to 1min segments set the the everage power for each segment.
+
+### Corner cases:
+. This calculation will sometimes result in the need to do negative power. The power is set to 0 in these cases. Usually 
+it is only slightly negative.
+. The per second ramp power workout should exactly match the critical power profile if ridden. (except for this issue above)
+. The workout created with the 1min segments, is a close approximation of the critical power profile.
+
+## Get a workout:
+. Zwift workouts are defined as a % of ftp. You will get 3 workouts,
+  . ftp = The value you define below will be used to override your zwift value during the workout.
+  . ftp = 1. In the case the workout will define the power you must ride, i.e. 120% of ftp == 120watts. Kinda a hack to set the watts.
+  . ftp = user (zwift) defined ftp. The workout will be defined as a % the provided ftp but when you ride it will use your zwift ftp.
+. You can define as many (second, watts) points as you like. Starting with 1sec and ending with 1200sec.(20min) or more.
+. Result: 4 files, the 3 workouts and the full data as a csv file.
+"""
+
+with st.form(key="ramp_test"):
+    st.markdown("#### Rider Profile")
+    name = st.text_input("Name", value="John and Jill")
+    ftp = st.number_input("FTP", min_value=10.0, max_value=1000.0, value=250.0)
+    st.markdown("##### Profile")
+    prof = st.text_area(
+        label="Profile [sec, watts]", value="1, 1000\n5, 800\n30, 500\n60, 450\n300, 400\n1200, 350", height=200
+    )
+    submit = st.form_submit_button("Submit")
+
+if submit:
+    with st.spinner("Processing..."):
+        profile = prof.split("\n")
+        profile = [x.split(",") for x in profile]
+        profile = [(int(x[0]), int(x[1])) for x in profile]
+        print(profile)
+        df, dfwko = ramp_test_activity(profile, ftp=ftp)
+        wko1 = make_zwo_from_ramp(dfwko, filename=None, name=f"{name}_ftp_{ftp}", ftp=ftp)
+        wko2 = make_zwo_from_ramp(dfwko, filename=None, name=f"{name}_ftp_1", ftp=1)
+        wkonone = make_zwo_from_ramp(dfwko, filename=None, name=f"{name}_ftp_1", ftp=None)
+
+        st.download_button(
+            label="Full data as CSV",
+            data=df[[c for c in df.columns if "power_seconds" not in c]].to_csv(index=False).encode("utf-8"),
+            file_name=f"ramp_test_{name}.csv",
+            mime="text/csv",
+        )
+        st.download_button(
+            label=f"Download WKO with ftp set to {ftp}",
+            data=wko1.encode("utf-8"),
+            file_name=f"ramp_test_{name}_ftp_{ftp}.zwo",
+            mime="text/csv",
+        )
+        st.download_button(
+            label="Download WKO with ftp set to 1",
+            data=wko2.encode("utf-8"),
+            file_name=f"ramp_test_{name}_ftp_1.zwo",
+            mime="text/csv",
+        )
+        st.download_button(
+            label="Download WKO with ftp set to in game ftp",
+            data=wkonone.encode("utf-8"),
+            file_name=f"ramp_test_{name}_ftp_user.zwo",
+            mime="text/csv",
+        )
---
+++ ramp_test.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+
+def ramp_test_activity(
+    profile: list[tuple[int:float]], segment_time: int = 30, test_length: int = 1200, ftp: int = 1
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Convert a power profile to a ramp test workout
+    The last 30sec is always 1 second ramp.
+    profile: list of tuples of seconds and power [(1, 100), (2, 90), (3, 85), (4, 80)] must start with 1sec
+    segment_time: int, the time in seconds of each segment of the ramp test workout, starting after 30sec
+    :return: full dataframe, workout segment dataframe, workout segment dataframe with power per ftp
+    """
+    try:
+        assert profile[0][0] == 1
+    except AssertionError:
+        raise ValueError("The profile must start with 1 second")
+    try:
+        assert profile[-1][0] >= test_length
+    except AssertionError:
+        raise ValueError("The profile must end with with a time >= test_length")
+    df = pd.DataFrame(profile, columns=["seconds", "power"])
+    df.set_index("seconds", inplace=True)
+    # fill in missing seconds
+    new_index = pd.Index(range(1, profile[-1][0] + 1), name="seconds")
+    df = df.reindex(new_index)
+    # Interpolate power for missing seconds
+    df["power"] = df["power"].interpolate(method="linear")
+    df.reset_index(inplace=True)
+    df = df[df.seconds <= test_length].copy()
+    df["power_seconds"] = df["power"] * df["seconds"]
+    df["ramp_power"] = 0
+    df["ramp_power"] = (df["power_seconds"] - df["power_seconds"].shift(1, fill_value=0)).clip(lower=0)
+    df["bins"] = df.apply(lambda row: row.name // segment_time + 30 if int(row.name) > 30 else row.name, axis=1)
+    df["bin_power"] = df.groupby("bins")["ramp_power"].transform("mean").round(0)
+    df["bin_time"] = df.groupby("bins")["seconds"].transform("count")
+    df["power_per_ftp"] = df["power"] / ftp
+    df["ramp_power_per_ftp"] = df["ramp_power"] / ftp
+    df["bin_power_per_ftp"] = df["bin_power"] / ftp
+    df.drop(columns=["power_seconds"], inplace=True)
+    df_wko = df.drop_duplicates(subset=["bins"], keep="first")
+    df_wko = df_wko[["bins", "bin_time", "bin_power", "bin_power_per_ftp"]].copy()
+    df_wko.rename(
+        columns={"bin_power": "power", "bin_time": "duration", "bin_power_per_ftp": "power%ftp"}, inplace=True
+    )
+    df_wko.sort_values(by="bins", ascending=False, inplace=True)
+    df_wko.reset_index(drop=True, inplace=True)
+    df_wko["segment"] = df_wko.index + 1
+    return df, df_wko[["segment", "duration", "power", "power%ftp"]]
+
+
+def make_zwo_from_ramp(workout: pd.DataFrame, filename: str | None, name: str, ftp: int | None = 1):
+    xml = ""
+    xml += "<workout_file>\n"
+    xml += "  <author>Vincent Davis</author>\n"
+    xml += f"  <name>Most Painful Ramp Test {name}</name>\n"
+    xml += "  <description>A ramp test based on power profile</description>\n"
+    xml += "  <sportType>bike</sportType>\n"
+    xml += "  <tags></tags>\n"
+    if ftp:
+        xml += f"  <ftpOverride>{ftp}</ftpOverride>\n"
+    xml += "  <workout>\n"
+    for r in workout.to_dict(orient="records"):
+        xml += f'      <SteadyState Duration="{r['duration']}" Power="{r['power%ftp']}"/>\n'
+    xml += "    </workout>\n"
+    xml += "</workout_file>\n"
+    if filename:
+        with open(filename, "w") as f:
+            f.write(xml)
+    return xml
